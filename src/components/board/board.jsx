2fb4ab4cecd5eba2872ef9615d55ad7faf20bd11
Rajeev Tikka <rajeevtikka@yahoo.com>
2021-06-20 15:10:33 -0400

    added game winning

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -1,25 +1,33 @@
 import React, {useState} from 'react';
 import Bubble from '../bubble/bubble'
 import './board.css'
+import {calculateWinner} from '../../helper'
 
 const Board = () => {
     const [markedArray, setArray] = useState(Array(16).fill(null))
     const [nextPlayer, setPlayer] = useState(true)
-    console.log(markedArray)
+    
+    const winner = calculateWinner(markedArray)
+    let status 
+    if (winner) status = 'winner' + winner;
 
     const handleClick = (point) => {
         const squares = [...markedArray]
+        if (calculateWinner(squares) || squares[point]) return
         squares[point] = nextPlayer ? '-blue': '-pink'
         setArray(squares)
         setPlayer(!nextPlayer)
     }
 
     return ( 
+        <div>
+        <h2> {status} </h2>
         <div className = "Board">
-            {markedArray.map((arr, i) => {
-                return (<Bubble key = {i} colorChange = {markedArray[i]} onclick = {() => handleClick(i)}/>)
-            }  
+            {markedArray.map((arr, i) => (
+                <Bubble key = {i} colorChange = {markedArray[i]} onclick = {() => handleClick(i)}/>
+            ) 
             )}
+        </div>
         </div>
     );
 }
